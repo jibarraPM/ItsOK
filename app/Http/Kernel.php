b5a2fccb0b7e9c5d5a60eb80b44318d441ecb663
SrDeLorean <xebaelvemgador@gmail.com>
2021-06-16 18:44:38 -0400

    Bloqueo de rutas por rol
    Se agrego un prefijo de su respectivo rol, si se me te a una pagina que no existe se va al 404, si se mete a una pagina que no tiene rango es redireccionado a su dashboard.

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -54,6 +54,9 @@ class Kernel extends HttpKernel
      */
     protected $routeMiddleware = [
         'auth' => \App\Http\Middleware\Authenticate::class,
+        'desarrollador' => \App\Http\Middleware\DesarrolladorMiddleware::class,
+        'administrador' => \App\Http\Middleware\AdministradorMiddleware::class,
+        'usuario' => \App\Http\Middleware\UsuarioMiddleware::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
---
+++ app/Http/Middleware/DesarrolladorMiddleware.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+
+class DesarrolladorMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $user = Auth::user();
+        if ($user->role != 1) {
+            $user = Auth::user();
+            if ($user->role == 1) {
+                return redirect("/desarrollador");
+            }
+            if ($user->role == 2) {
+                return redirect("/administrador");
+            }
+            if ($user->role == 3) {
+                return redirect("/usuario");
+            }
+            return redirect("/login");
+        }
+        return $next($request);
+    }
+}
